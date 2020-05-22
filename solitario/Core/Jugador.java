25e9e9551d9c33b839494522794ea2aaa6e6a1cd
Javier <javiertoyos1@gmail.com>
2020-05-22 12:41:46 +0200

    Update

--- solitario/Core/Jugador.java
+++ solitario/Core/Jugador.java
@@ -1,7 +1,7 @@
 /*
-  *  Representa al único jugador de la partida, identificado por el nombre
-  * Funcionalidad: le da la vuelta a una carta que está boca abajo, escoge una carta para moverla o al montón de descarte
-  *                o la mueve encima de otra carta del interior
+ *  Representa al único jugador de la partida, identificado por el nombre
+ * Funcionalidad: le da la vuelta a una carta que está boca abajo, escoge una carta para moverla o al montón de descarte
+ *                o la mueve encima de otra carta del interior
  */
 package Core;
 
